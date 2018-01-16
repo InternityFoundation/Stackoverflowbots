ed503725c099184688182e9122bfb9aeb1d904c8
Shivansh <shiv4nsh@gmail.com>
2018-01-16 12:36:27 +0530

    Corrected the getError

--- src/main/scala/in/internity/datasource/SaveConfigurationsDB.scala
+++ src/main/scala/in/internity/datasource/SaveConfigurationsDB.scala
@@ -78,7 +78,7 @@ object SaveConfigurationsDB {
 
       def next() = {
         (Slack(rs.getString(1),rs.getString(2)),
-          rs.getString(4),rs.getDouble(3))
+          rs.getString(3),rs.getDouble(4))
       }
     }.toStream
     val list = stream.toList
